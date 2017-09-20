43433e11b90a9c0250f8cf00aab177b56331538e
jwolfe890 <jwolfe890@gmail.com>
2017-09-20 00:58:40 -0400

    changed name

--- stumped-app-client/src/App.js
+++ stumped-app-client/src/App.js
@@ -5,7 +5,9 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>SHOPPER'S PARADISE</h1>
+        <div>
+          <Products products={['products']}/>
+        </div>
       </div>
     );
   }
