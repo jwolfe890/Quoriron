0160e77a1c4ba34d507cd45ae983504b9de6f1b7
jwolfe890 <jwolfe890@gmail.com>
2017-09-22 02:42:03 -0400

    added create controller

--- stumped-app-api/app/controllers/api/questions_controller.rb
+++ stumped-app-api/app/controllers/api/questions_controller.rb
@@ -3,6 +3,20 @@ class Api::QuestionsController < ApplicationController
   def index
     @questions = Question.all 
     render json: @questions
-  end 
+  end
+
+  def create
+    @question = Question.new(question_params)
+    if @question.save
+      render json: @question
+    else 
+      render json: { errors: { message: "This question failed to save" } }
+    end
+  end
+
+private
+  def question_params
+    params.require(:question).permit(:title)
+  end
 
 end 
