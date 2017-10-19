738a825cc73f77d8cfa26abb21d2eeba1cb0fad1
jwolfe890 <jwolfe890@gmail.com>
2017-10-18 23:39:03 -0400

    fixed glitch

--- quoriron-app-client/src/components/Home.js
+++ quoriron-app-client/src/components/Home.js
@@ -1,11 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom'
 
-<<<<<<< HEAD
-const Home = () => {  
-=======
 const Home = () => {
->>>>>>> testing
     return (
         <div>
           <h4>Welcome to Quorion! Sign Up is coming soon.</h4> 
