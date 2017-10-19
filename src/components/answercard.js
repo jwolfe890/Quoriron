4fefbee44e6b8fce2ac04ad9d8244ea4c82cb48e
jwolfe890 <jwolfe890@gmail.com>
2017-10-18 23:39:03 -0400

    fixed glitch

--- src/components/answercard.js
+++ src/components/answercard.js
@@ -1,19 +1,10 @@
 import React, { Component } from 'react';
 
-<<<<<<< HEAD
-
-const AnswerCard = ({ content, rating, upvoter, downvoter }) => { 
-    return (
-        <div>
-          <p>{content}</p>
-          AWESOME RATING: <p>{rating}</p>
-=======
 const AnswerCard = ({content, rating, upvoter, downvoter}) => { 
     return (
         <div>
           <p>{content}</p>
           <p className="rating">HELPFULNESS RATING: {rating}</p>
->>>>>>> testing
           <button onClick={upvoter}>Upvote</button>
           <button onClick={downvoter}>Downvote</button>
         </div>
