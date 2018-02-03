7a9002810acbf441eed283346c2baa2f3e8a712a
John Wolfe <jwolfe890@gmail.com>
2018-02-03 05:42:00 -0500

    Refactored Component
    Removed logic from the return statement and moved it to render to improve clarity of return. Also added React 16.2 Fragment to remove the enclosing div tags.

--- quoriron-app-client/src/containers/answers.js
+++ quoriron-app-client/src/containers/answers.js
@@ -1,8 +1,7 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import { connect } from 'react-redux';
 import { getAnswers } from '../actions/answers'
 import Answer from './answer'
-
 import { getQuestions } from '../actions/questions'
 
 
@@ -10,19 +9,20 @@ class Answers extends Component {
 
 render() {
   const { question } = this.props
-return (
-  <div>
-      { question ? question.answers.sort(function(a,b) 
-        { return (a.count < b.count) ? 1 : ((b.count > a.count) ? -1 : 0)} 
-          ).map(answer =>  
-            <Answer key={answer.id} answer={answer} questionId={question.id} />
-          ) : 
-        <p>Loading...</p>
-        }
-  </div>  
-    );
-  }    
-}
+  let answersCard = null
 
+  question ? 
+    sortedAnswers = ( question.answers.sort(function(a,b) { return (a.count < b.count) ? 1 : ((b.count > a.count) ? -1 : 0)} 
+    ).map(answer =>  
+    <Answer key={answer.id} answer={answer} questionId={question.id} />
+    )) : sortedAnswers = ( <p>Loading...</p> )
+    
+return (
+    <Fragment>
+      {sortedAnswers}
+    </Fragment> 
+   )
+  }
+}
 
 export default connect(null, { getAnswers, getQuestions })(Answers);
