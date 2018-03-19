337bbe6057b31002f1d2c2559e01ac088e15cc15
John Wolfe <johnwolfe@Johns-MacBook-Pro-2.local>
2018-03-19 08:49:00 -0400

    new version

--- quoriron-app-client/src/components/Home.js
+++ quoriron-app-client/src/components/Home.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom'
+import { Button } from 'react-bootstrap';
 
 const Home = () => {
     return (
@@ -7,7 +8,7 @@ const Home = () => {
           <h4>Welcome to Quorion! Sign Up is coming soon.</h4> 
           <p>But for now, why don't you answer or ask some questions!</p>
           <Link style={{ marginRight: '5px', color: 'white' }} to={`/questions`}><button style={{ backgroundColor: '#009933', borderColor: '#009933' }}>Answer Questions</button></Link>
-          <Link style={{ color: 'white' }} to={`/questions/new`}><button style={{ backgroundColor: '#b92b27', borderColor: '#b92b27' }}>Ask Question</button></Link>
+          <Link style={{ color: 'white' }} to={`/questions/new`}><Button style={{ backgroundColor: '#b92b27', borderColor: '#b92b27' }}>Ask Question</Button></Link>
         </div>
     );
 }
--- quoriron-app-client/src/index.js
+++ quoriron-app-client/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-
+import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
