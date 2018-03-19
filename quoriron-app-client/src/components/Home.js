be3df0d0f3e4011088ed8130397495bf905e1483
John Wolfe <johnwolfe@Johns-MacBook-Pro-2.local>
2018-03-19 08:52:04 -0400

    added buttons

--- quoriron-app-client/src/components/Home.js
+++ quoriron-app-client/src/components/Home.js
@@ -7,7 +7,7 @@ const Home = () => {
         <div>
           <h4>Welcome to Quorion! Sign Up is coming soon.</h4> 
           <p>But for now, why don't you answer or ask some questions!</p>
-          <Link style={{ marginRight: '5px', color: 'white' }} to={`/questions`}><button style={{ backgroundColor: '#009933', borderColor: '#009933' }}>Answer Questions</button></Link>
+          <Link style={{ marginRight: '5px', color: 'white' }} to={`/questions`}><Button style={{ backgroundColor: '#009933', borderColor: '#009933' }}>Answer Questions</Button></Link>
           <Link style={{ color: 'white' }} to={`/questions/new`}><Button style={{ backgroundColor: '#b92b27', borderColor: '#b92b27' }}>Ask Question</Button></Link>
         </div>
     );
